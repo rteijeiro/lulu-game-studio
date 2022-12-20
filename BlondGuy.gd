8560da7a1aebe278a63b8cb7d5164c8f3845add2
BelenUrbina <lopezurbinabelen@gmail.com>
2022-12-20 16:30:36 +0100

    Removed DialogueManager Plugin

--- BlondGuy.gd
+++ BlondGuy.gd
@@ -13,14 +13,7 @@ func _process(delta):
 
 func _input(event):
 	if event.is_action_pressed("bark") and len(get_overlapping_bodies()) > 0:
-		use_dialogue()
-
-func use_dialogue():
-	var resource = preload("res://Dialogues/Dialogue.tres")
-	DialogueManager.show_example_dialogue_balloon(\
-		"this_is_a_node_title", \
-		resource
-	)
+		pass
 
 
 func _on_BlondGuy_body_entered(body):
