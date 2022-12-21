db5020846faddc02040b53dd78f3aa00d7e4873b
BelenUrbina <lopezurbinabelen@gmail.com>
2022-12-21 13:52:25 +0100

    New Dialog activated when Lulu is closed to the Character BlondeGuy

--- Sprites/Characters/BlondGuy/BlondGuy.gd
+++ Sprites/Characters/BlondGuy/BlondGuy.gd
@@ -1,20 +1,21 @@
 extends Area2D
 
-
-var active = false 
-
-func _process(delta):
-	$InteractionMessage.visible = active
-
 func _on_BlondGuy_body_entered(body):
 	if body.name == "Lulu":
-		active = true
+		var new_dialog = Dialogic.start('first')
+		add_child(new_dialog)
+		new_dialog.connect("timeline_end", self, 'after_dialog')
+		$InteractionMessage.visible = true
+		
+	
 		
 func _on_BlondGuy_body_exited(body):
 	if body.name == "Lulu":
-		active = false 
+		$InteractionMessage.visible = false 
 
 
+func after_dialog(timeline_name):
+	print("You can resume with the game")
 
 
 
