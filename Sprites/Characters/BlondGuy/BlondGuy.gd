e5031b20e1d0cc3cb773c3c0fd22cf1d22114aef
BelenUrbina <lopezurbinabelen@gmail.com>
2022-12-21 10:33:25 +0100

    File BlondGuy.gd cleaned. Advertaisement has now the name Hamburguer and is not a scene anymore

--- Sprites/Characters/BlondGuy/BlondGuy.gd
+++ Sprites/Characters/BlondGuy/BlondGuy.gd
@@ -3,18 +3,8 @@ extends Area2D
 
 var active = false 
 
-func _ready():
-	connect("body_entered", self, '_on_BlondGuy_body_entered')
-	connect("body_excited", self, '_on_BlondGuy_body_exited')
-
 func _process(delta):
 	$InteractionMessage.visible = active
-
-
-func _input(event):
-	if event.is_action_pressed("bark") and len(get_overlapping_bodies()) > 0:
-		pass
-
 
 func _on_BlondGuy_body_entered(body):
 	if body.name == "Lulu":
