90cf20a14c081d7c9220ad0f74cb2e9ba364d2f1
BelenUrbina <lopezurbinabelen@gmail.com>
2022-12-21 14:06:22 +0100

    The dialog with the character appears only one time. While they are talking, Lulu can't move

--- Sprites/Characters/BlondGuy/BlondGuy.gd
+++ Sprites/Characters/BlondGuy/BlondGuy.gd
@@ -1,21 +1,24 @@
 extends Area2D
 
+onready var lulu = get_node("/root/World/Lulu")
+
+var showed:bool = false
+
 func _on_BlondGuy_body_entered(body):
-	if body.name == "Lulu":
+	if body.name == "Lulu" and !showed:
 		var new_dialog = Dialogic.start('first')
 		add_child(new_dialog)
 		new_dialog.connect("timeline_end", self, 'after_dialog')
 		$InteractionMessage.visible = true
-		
-	
+		lulu.set_physics_process(false)
+		showed = true
 		
 func _on_BlondGuy_body_exited(body):
 	if body.name == "Lulu":
 		$InteractionMessage.visible = false 
 
-
 func after_dialog(timeline_name):
-	print("You can resume with the game")
+	lulu.set_physics_process(true)
 
 
 
